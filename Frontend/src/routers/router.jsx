fb362eec069f32e17847a0c923c36d88451ebce8
orun-sunny <sunny.mukut@gmail.com>
2025-01-24 18:47:09 +0600

    added last

--- Frontend/src/routers/router.jsx
+++ Frontend/src/routers/router.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
 import CartPage from "../pages/Books/CartPage";
+import CheckoutPage from "../pages/Books/Checkout";
 
 const router = createBrowserRouter([
   {
@@ -37,6 +38,10 @@ const router = createBrowserRouter([
       {
         path: "/cart",
         element: <CartPage />,
+      },
+      {
+        path: "/checkout",
+        element: <CheckoutPage />,
       },
     ],
   },
