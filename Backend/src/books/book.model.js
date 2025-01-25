b4cf5190bd6478dd12b24ddaa528213f5bf47fce
orun-sunny <sunny.mukut@gmail.com>
2025-01-25 16:06:29 +0600

    working on api server

---
+++ Backend/src/books/book.model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    _id: {
+        type: Number,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+
+    },
+    trending: {
+        type: Boolean,
+        default: false,
+    },
+    coverImage: {
+        type: String,
+        required: true,
+    },
+    oldPrice: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    newPrice: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+}, { timestamps: true });
+const Book = mongoose.model('Book', bookSchema);
+module.exports = Book;
