f551c8776e2662c23b2e1628417afe91bc82d3f4
orun-sunny <sunny.mukut@gmail.com>
2025-01-25 11:30:29 +0600

    added server

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,29 @@
+require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose');
 const app = express()
 const port = process.env.PORT || 5000
 
+
+
+
+
+
+
+
+
+
+
+
+async function main() {
+    await mongoose.connect(process.env.DB_URL);
+    app.use("/", (req, res) => {
+        res.send("Book Store Server is running!");
+    });
+}
+
+main().then(() => console.log("Mongodb connect succesfully")).catch(err => console.log(err));
+
+app.listen(port, () => {
+    console.log(`Server is running on port " ${port}`);
+})
